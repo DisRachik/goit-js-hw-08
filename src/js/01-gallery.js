9a6c4fc56a4ad4746334d6baf4f84fed1db60908
DisRachik <radchenkoig@gmail.com>
2023-03-02 16:52:24 +0200

    the first task is completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = placeImageInGallery(galleryItems);
+
+function placeImageInGallery(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<a class='gallery__item' href='${original}'>
+            <img class="gallery__image" src="${preview}" alt="${description}">
+          </a>`
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoomFactor: 0.05,
+});
