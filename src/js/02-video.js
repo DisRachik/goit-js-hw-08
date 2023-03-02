ff4161e59354db3d7a47e0c0b56129ecefeddd0e
DisRachik <radchenkoig@gmail.com>
2023-03-02 18:49:28 +0200

    the second task is completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Vimeo from '@vimeo/player';
+import _throttle from 'lodash.throttle';
 
+const VIDEO_TIME_KEY_STORAGE = 'videoplayer-current-time';
+
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframeEl);
+
+const onRememberTimeVideo = ({ seconds }) => {
+  localStorage.setItem(VIDEO_TIME_KEY_STORAGE, seconds);
+};
+
+player.setCurrentTime(localStorage.getItem(VIDEO_TIME_KEY_STORAGE));
+player.on('timeupdate', _throttle(onRememberTimeVideo, 1000));
